be805fd98e04d4f8cd4c90513f87335e5fa03adc
r. ávila <avila.dev@outlook.com>
2025-01-08 20:54:43 -0300

    feat: re-implement inputs reading

--- src/txx/inputs/input.rs
+++ src/txx/inputs/input.rs
@@ -1,65 +1,98 @@
 use super::{errors::ReadCompactSizeError, ReadInputsError, ReadInputsLengthError};
-use crate::txx::{self, TransactionBytesTrait};
+use crate::txx::bytes::{TransactionBytesTrait, TransactionData};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
 pub struct Input {
-    _txid: [u8; 32],
-    _vout: [u8; 4],
-    _script_sig: Vec<u8>,
-    _sequence: [u8; 4],
+    pub txid: String,
+    pub output_index: u32,
+    pub script_sig: String,
+    pub sequence: u32,
+}
+
+impl std::fmt::Debug for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Input")
+            .field("txid", &self.txid)
+            .field("vout", &self.output_index)
+            .field("script_sig", &self.script_sig)
+            .field("sequence", &self.sequence)
+            .finish()
+    }
 }
 
 pub type InputsLength = u64;
 
 pub const INPUT_LENGTH_POSITION: usize = 4;
-pub const INPUT_TXID_POSITION: usize = 5;
-pub const INPUT_VOUT_POSITION: usize = INPUT_TXID_POSITION + 32 + 1;
-pub const INPUT_SCRIPT_SIG_POSITION: usize = INPUT_VOUT_POSITION + 1;
 
-pub fn from(tx_bytes: &txx::TransactionData) -> Result<Vec<Input>, ReadInputsError> {
+pub fn from(tx_bytes: &TransactionData) -> Result<Vec<Input>, ReadInputsError> {
+    let (mut inputs, mut position) = (Vec::new(), INPUT_LENGTH_POSITION + 1);
+
     let length: InputsLength = match tx_bytes.length() {
         Ok(length) => length,
         Err(err) => return Err(ReadInputsError::InvalidData(err)),
     };
 
-    let mut inputs = Vec::new();
     for _ in 0..length {
-        let txid: [u8; 32] = tx_bytes[INPUT_TXID_POSITION..(INPUT_TXID_POSITION + 32)]
+        if position + 36 > tx_bytes.len() {
+            return Err(ReadInputsError::DataCorruption);
+        }
+
+        // txid: [32 bytes]
+        let mut buffer: [u8; 32] = tx_bytes[position..position + 32]
+            .try_into()
+            .map_err(|_| ReadInputsError::DataCorruption)?;
+        buffer.reverse();
+        let txid = hex::encode(buffer);
+
+        position += 32;
+
+        // vout: [4 bytes]
+        let vout: [u8; 4] = tx_bytes[position..position + 4]
             .try_into()
             .map_err(|_| ReadInputsError::DataCorruption)?;
 
-        let vout: [u8; 4] = tx_bytes[INPUT_VOUT_POSITION..4]
-            .try_into()
-            .map_err(|_| ReadInputsError::DataCorruption)?;
+        position += 4;
 
-        let (script_sig_head, script_sig_tail) = (
-            tx_bytes[INPUT_SCRIPT_SIG_POSITION],
-            &tx_bytes[INPUT_SCRIPT_SIG_POSITION + 1..],
-        );
+        // scriptSig
+        let (script_sig_head, script_sig_tail) = (tx_bytes[position], &tx_bytes[position..]);
+        position += 1;
 
         let script_sig_size = match read_compact_size(script_sig_head, script_sig_tail) {
-            Ok(size) => size,
+            Ok(size) => size as usize,
             Err(_) => return Err(ReadInputsError::DataCorruption),
         };
 
-        let script_sig: Vec<u8> =
-            tx_bytes[INPUT_SCRIPT_SIG_POSITION..(script_sig_size as usize)].to_vec();
+        if position + script_sig_size > tx_bytes.len() {
+            return Err(ReadInputsError::DataCorruption);
+        }
 
-        let sequence: [u8; 4] = tx_bytes[(INPUT_SCRIPT_SIG_POSITION + script_sig_size as usize)..4]
+        let script_sig = &tx_bytes[position..position + script_sig_size];
+        position += script_sig_size;
+
+        // sequence: [4 bytes]
+        if position + 4 > tx_bytes.len() {
+            return Err(ReadInputsError::DataCorruption);
+        }
+
+        let sequence: [u8; 4] = tx_bytes[position..position + 4]
             .try_into()
             .map_err(|_| ReadInputsError::DataCorruption)?;
 
+        position += 4;
+
         inputs.push(Input {
-            _txid: txid,
-            _vout: vout,
-            _script_sig: script_sig,
-            _sequence: sequence,
+            txid,
+            output_index: u32::from_le_bytes(vout),
+            script_sig: hex::encode(script_sig),
+            sequence: u32::from_le_bytes(sequence),
         });
     }
 
-    Ok(vec![])
+    Ok(inputs)
 }
 
-pub fn length(tx_bytes: &txx::TransactionData) -> Result<InputsLength, ReadInputsLengthError> {
+pub fn length(tx_bytes: &TransactionData) -> Result<InputsLength, ReadInputsLengthError> {
     let size = tx_bytes.len();
     if size < INPUT_LENGTH_POSITION + 1 {
         return Ok(0);
