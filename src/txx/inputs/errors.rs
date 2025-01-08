db8409871326bb25c41d9b79874f13cb00e5388a
r. ávila <avila.dev@outlook.com>
2025-01-08 00:38:48 -0300

    feat: create ReadInputsError

--- src/txx/inputs/errors.rs
+++ src/txx/inputs/errors.rs
@@ -1,5 +1,35 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ReadInputsLengthError {
+pub enum ReadInputsError {
+    /// Invalid data.
+    InvalidData(ReadCompactSizeError),
+
+    /// Corrupted data when parsing bytes.
+    DataCorruption,
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for ReadInputsError {}
+
+impl std::fmt::Display for ReadInputsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ReadInputsError::InvalidData(err) => write! {
+                f,
+                "Failed to define input's length: {}",
+                err
+            },
+            ReadInputsError::DataCorruption => write! {
+                f,
+                "Transaction data is corrupted or contains invalid byte sequences."
+            },
+        }
+    }
+}
+
+pub type ReadInputsLengthError = ReadCompactSizeError;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ReadCompactSizeError {
     /// Insufficient data to extract the input count.
     InsufficientData { length: usize },
 
@@ -8,17 +38,17 @@ pub enum ReadInputsLengthError {
 }
 
 #[cfg(feature = "std")]
-impl std::error::Error for ReadInputsLengthError {}
+impl std::error::Error for ReadCompactSizeError {}
 
-impl std::fmt::Display for ReadInputsLengthError {
+impl std::fmt::Display for ReadCompactSizeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ReadInputsLengthError::InsufficientData { length } => write! {
+            ReadCompactSizeError::InsufficientData { length } => write! {
                 f,
                 "Transaction data is too short to extract the inputs' length (length: {}, should be at least {})",
                 length, super::INPUT_LENGTH_POSITION + 1
             },
-            ReadInputsLengthError::DataCorruption => write! {
+            ReadCompactSizeError::DataCorruption => write! {
                 f,
                 "Transaction data is corrupted or contains invalid byte sequences."
             },
